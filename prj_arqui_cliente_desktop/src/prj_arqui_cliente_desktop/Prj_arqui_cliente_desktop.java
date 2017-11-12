ba71a6316acb17bc3a43274ec07268a539c12119
marcorod94 <marco.rod.94@live.com>
2017-11-12 12:19:39 -0500

    Creacion de Proyecto Arqui

---
+++ prj_arqui_cliente_desktop/src/prj_arqui_cliente_desktop/Prj_arqui_cliente_desktop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prj_arqui_cliente_desktop;
+
+/**
+ *
+ * @author Marco
+ */
+public class Prj_arqui_cliente_desktop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
